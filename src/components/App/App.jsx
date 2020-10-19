626cd22aa3412edd5beb559f4c5fbef8f9acab3e
Koala <zmiyov2422@gmail.com>
2020-10-19 15:30:37 +0300

    Create task

---
+++ src/components/App/App.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PNotify from 'pnotify/dist/es/PNotify';
+import styles from './app.module.css';
+import ContactForm from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
+import { filterContacts, findToMatch } from '../FilterContact/filterContact';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addContact = contact => {
+    const findContact = findToMatch(this.state.contacts, contact);
+    if (contact.name) {
+      // eslint-disable-next-line no-unused-expressions
+      findContact
+        ? PNotify.alert(`${findContact.name} is already in contacts`)
+        : this.setState(prevState => ({
+            contacts: [...prevState.contacts, contact],
+          }));
+    } else {
+      PNotify.error({
+        text: "'Input name!'",
+      });
+    }
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = filterContacts(contacts, filter);
+
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm
+          onChange={this.handleChange}
+          onAddContact={this.addContact}
+        />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.handleChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDelete={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
